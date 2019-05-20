a15313a3596a2d22bddc145dec5a3a15c9599133
Alexandr Balasyan <mywebstreet@gmail.com>
2019-05-20 15:16:36 +0300

    audit

--- dist/localization.min.js
+++ dist/localization.min.js
@@ -1,7 +1,7 @@
 /*!
  * Localization module
  * 
- * @version 2.0.4
+ * @version 2.0.5
  * @author Alexandr Balasyan <mywebstreet@gmail.com>
  * {@link https://github.com/ortexx/localizationjs}
  */
