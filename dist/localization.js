7dd5a42826fc6a176a6b51c31ee4fb118e00254b
Alexandr Balasyan <mywebstreet@gmail.com>
2022-09-08 15:43:21 +0300

    upgraded deps

--- dist/localization.js
+++ dist/localization.js
@@ -1,7 +1,7 @@
 /*!
  * Localization module
  *
- * @version 2.0.20
+ * @version 2.0.21
  * @author Alexandr Balasyan <mywebstreet@gmail.com>
  * {@link https://github.com/ortexx/localizationjs}
  */
